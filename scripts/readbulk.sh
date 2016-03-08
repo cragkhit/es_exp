45e5faf5d919b3903f74c16745c405e637859247
Chaiyong Ragkhitwetsagul <ucabagk@ucl.ac.uk>
2016-03-08 21:35:49 +0000

    Updated read to readbulk which can index multiple docs

--- scripts/readbulk.sh
+++ scripts/readbulk.sh
@@ -8,11 +8,12 @@ do
     #whatever you need with "$file"
     id=`echo $file | sed -e "s:$1\/::" | sed -e 's/.java//'`
     src=`java -jar "$TOKENIZER" -f $file -l $4 -v $5 -n $6`
-    printf $file" "
+    #printf $file" "
     #echo "curl -XPUT 'http://localhost:9200/$2/$id' -d '{ \"id\" : \"$id\", \"src\" : \"$src\"}'"
     #curl -XPUT "http://localhost:9200/$2/$id?pretty" -d "{ \"id\" : \"$id\", \"src\" : \"$src\"}"
-    echo '{ "index":  { "_index": "'$2'", "_type": "'$3'" }}\n' > bulk.query
-    echo '{ "src":    "'$src'" }' >> bulk.query
-    curl -XPOT -s -XPOST localhost:9200/_bulk --data-binary "@requests"; bulk.query
-    #rm bulk.query
+    echo "{ \"index\": { \"_index\": \"$2\", \"_type\": \"$3\", \"_id\": \"$id\" }}" >> bulk.query
+    echo "{ \"src\": \"$src\" }" >> bulk.query
 done
+
+curl -XPOT -s -XPOST localhost:9200/_bulk --data-binary @bulk.query; echo
+rm bulk.query
