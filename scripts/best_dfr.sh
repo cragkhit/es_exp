af87c70c169466fc2653549e2803cbd9e07ad5d8
Ubuntu <cragkhit@ecs2.jr3cimwvz5yehkrb4ewqepxqjh.cx.internal.cloudapp.net>
2016-03-14 20:38:17 +0000

    Modified DFR to accept n value of ngram, and fixed combine_csv script to show usage

--- scripts/best_dfr.sh
+++ scripts/best_dfr.sh
@@ -1,5 +1,5 @@
 if [ "$#" -eq 0 ]; then
-        echo "Usage: ./run_all.sh <server name> <input folder> <ES doc type> <index prefix> <normalization>" 
+        echo "Usage: ./run_all.sh <server name> <input folder> <ES doc type> <index prefix> <normalization> <ngram size>" 
         exit 1
 fi
 
@@ -11,13 +11,13 @@ for basic_model in "be" "d" "g" "if" "in" "ine" "p"; do
           curl -XDELETE $1:9200/$final_index_name
           curl -XPUT $1:9200/$final_index_name -d '{ "settings": { "similarity": { "dfr_similarity" : { "type": "DFR", "basic_model": "'$basic_model'", "after_effect": "'$after_effect'", "normalization": "'$normalization'", "normalization.h2.c": "3.0" } } }, "mappings": {  "doc":{  "properties": {  "src": {  "type": "string",  "similarity": "dfr_similarity"  }  }  }  } ,  "index" : {  "analysis" : { "analyzer" : { "default" : { "type" : "whitespace" } } } } }'
 
-          ./scripts/readbulk.sh $2 "$final_index_name" "$3" $5 4 true
+          ./scripts/readbulk.sh $2 "$final_index_name" "$3" $5 $6 true
 
           dfs="-f"
-          output=results_dfr
+          output=best_param_dfr_$6
           mkdir $output
           #echo "java -jar tools/checker.jar -s $1 -i $final_index_name -t src -d $3 -l $i $ngram_flag $k 1> $output/$final_index_name.csv"
-          java -jar tools/checker.jar -s $1 -i $final_index_name -t $3 -d $2 -l $5 -n $dfs 1> $output/$5ngram_$final_index_name.csv
+          java -jar tools/checker.jar -s $1 -i $final_index_name -t $3 -d $2 -l $5 -g $6 -n $dfs 1> $output/$5ngram_$final_index_name.csv
           curl -XDELETE $1:9200/$final_index_name
         done
     done
--- scripts/combine_csv.sh
+++ scripts/combine_csv.sh
@@ -1,4 +1,12 @@
+if [ "$#" -eq 0 ]; then
+        echo "Usage: ./combin_csv.sh <directory> <output file> <unwanted prefix>" 
+        exit 1
+fi
+
 names=`ls $1/*.csv`
-rm $2
+
+# delete the file if it exists
+if [ -f $2 ]; then rm $2; fi
+
 echo $names | sed -e "s:.csv:,:g" | sed -e s:$3::g > $2
 paste -d ',' `echo $names` >> $2
