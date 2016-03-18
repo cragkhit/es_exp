14c35da60e959439193f82d90e3ec9ad07e22f7b
Chaiyong R <cragkhit@gmail.com>
2016-03-18 15:29:51 +0000

    Updated script to run all normalisation + settings + n-gram

--- scripts/best_bm25.sh
+++ scripts/best_bm25.sh
@@ -6,20 +6,20 @@ fi
 # create all possible index combinations
 for i in "0.0" "0.6" "1.2" "1.8" "2.4"; do
     for j in "0.0" "0.25" "0.50" "0.75" "1.00"; do
-        for k in "true" "false"; do
+#        for k in "true" "false"; do
             k1=`echo $i | sed -e s:\\\\.::`
             b=`echo $j | sed -e s:\\\\.::` 
             final_index_name=$4"_"$k1"_"$b"_"$k
             curl -XDELETE $1:9200/$final_index_name
-            curl -XPUT $1:9200/$final_index_name -d '{ "settings": { "similarity": { "bm25_similarity" : { "type": "BM25", "k1": "'$i'", "b": "'$j'", "discount_overlaps": "'$k'"} } }, "mappings": {  "doc":{  "properties": {  "src": {  "type": "string",  "similarity": "bm25_similarity"  }  }  }  } ,  "index" : {  "analysis" : { "analyzer" : { "default" : { "type" : "whitespace" } } } } }'
+            curl -XPUT $1:9200/$final_index_name -d '{ "settings": { "similarity": { "bm25_similarity" : { "type": "BM25", "k1": "'$i'", "b": "'$j'"} } }, "mappings": {  "doc":{  "properties": {  "src": {  "type": "string",  "similarity": "bm25_similarity"  }  }  }  } ,  "index" : {  "analysis" : { "analyzer" : { "default" : { "type" : "whitespace" } } } } }'
             ./scripts/readbulk.sh $2 "$final_index_name" "$3" $5 4 true
 
             dfs="-f"
-            output=results_bm25
+            output=best_param_bm25
             mkdir $output
-            java -jar tools/checker.jar -s $1 -i $final_index_name -t $3 -d $2 -l $5 -n $dfs 1> $output/$5ngram_$final_index_name.csv
+            java -jar tools/checker.jar -s $1 -i $final_index_name -t $3 -d $2 -l $5 -n $dfs 1> $output/$5ngram_$final_index_name"_no_do".csv
             curl -XDELETE $1:9200/$final_index_name
-        done
+#        done
     done
 done
 
--- scripts/best_default.sh
+++ scripts/best_default.sh
@@ -1,25 +1,47 @@
+# A script to run ES with the given normalisation
+# This script creates TF/IDF index with different parameter settings of discount_overlap (true/false/-)
+
 if [ "$#" -eq 0 ]; then
-        echo "Usage: ./run_all.sh <server name> <input folder> <ES doc type> <index prefix> <normalization>" 
+        echo "Usage: ./run_all.sh <server name> <input folder> <ES doc type> <index prefix> <normalization> <size of n> <output folder>" 
         exit 1
 fi
 
+    hostname=$1
+    input=$2
+    doctype=$3
+    index=$4
+    norm=$5
+    n=$6
+    output=$7
 # create all possible index combinations
 for i in "no" "true" "false"; do
-    final_index_name=$4"_"$i
-    curl -XDELETE $1:9200/$final_index_name
+    final_index_name="$index-$i-$norm-$n"
+    
+    echo "======================================================="
+    printf "HOST:\t\t\t$hostname\n"
+    printf "INDEX:\t\t\t$final_index_name/$doctype\n"
+    printf "NORM:\t\t\t$norm + $n-gram\n"
+    printf "SETTINGS:\t\t$i\n"
+    printf "DATA:\t\t\tfrom: $input to: $output\n"
+    echo "======================================================="
+
+    # delete the index if it exists
+    curl -XDELETE $hostname:9200/$final_index_name
+
     if [ $i == "no" ] 
     then 
-        curl -XPUT $1:9200/$final_index_name -d '{ "index" : { "analysis" : { "analyzer" : { "default" : { "type" : "whitespace" } } } } }'
+        curl -XPUT $hostname:9200/$final_index_name -d '{ "index" : { "analysis" : { "analyzer" : { "default" : { "type" : "whitespace" } } } } }'
     else
-        curl -XPUT $1:9200/$final_index_name -d '{ "settings": { "similarity": { "tfidf_similarity" : { "type": "default", "discount_overlaps": "'$i'"} } }, "mappings": {  "doc":{  "properties": {  "src": {  "type": "string",  "similarity": "tfidf_similarity"  }  }  }  } ,  "index" : {  "analysis" : { "analyzer" : { "default" : { "type" : "whitespace" } } } } }'
+        curl -XPUT $hostname:9200/$final_index_name -d '{ "settings": { "similarity": { "tfidf_similarity" : { "type": "default", "discount_overlaps": "'$i'"} } }, "mappings": {  "doc":{  "properties": {  "src": {  "type": "string",  "similarity": "tfidf_similarity"  }  }  }  } ,  "index" : {  "analysis" : { "analyzer" : { "default" : { "type" : "whitespace" } } } } }'
     fi
 
-    ./scripts/readbulk.sh $2 $final_index_name $3 $5 4 true
+    ./scripts/readbulk.sh $input $final_index_name $doctype $norm $n true
 
+    # use DFS mode
     dfs="-f"
-    output=best_param_tfidf
     mkdir $output
-    #echo "java -jar tools/checker.jar -s $1 -i $final_index_name -t src -d $3 -l $i $ngram_flag $k 1> $output/$final_index_name.csv"
-    java -jar tools/checker.jar -s $1 -i $final_index_name -t $3 -d $2 -l $5 $dfs -n 1> $output/$5ngram_$final_index_name.csv
-    curl -XDELETE $1:9200/$final_index_name
+    java -jar tools/checker.jar -s $hostname -i $final_index_name -t $doctype -d $input -l $norm $dfs -n -g $n 1> $output/$final_index_name.csv
+
+    # clean up by delete the index
+    curl -XDELETE $hostname:9200/$final_index_name
 done
