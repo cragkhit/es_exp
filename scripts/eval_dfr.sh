48cb70faaee53a8701ab0d430850854e497bc7ca
Chaiyong Ragkhitwetsagul <cragkhit@gmail.com>
2016-03-22 21:52:37 +0000

    Fixed the problem of inconsistent results (yeah~)

--- scripts/eval_dfr.sh
+++ scripts/eval_dfr.sh
@@ -29,11 +29,18 @@ final_index_name=$index"_"$basic_model"_"$after_effect"_"$normalization
 #printf "DATA:\t\t\tfrom: $input to: $output\n"
 #echo "======================================================="
 
-curl -s -XDELETE $hostname:9200/$final_index_name > /dev/null
+#curl -s -XDELETE $hostname:9200/$final_index_name > /dev/null
 curl -s -XPUT $hostname:9200/$final_index_name -d '{ "settings": { "similarity": { "dfr_similarity" : { "type": "DFR", "basic_model": "'$basic_model'", "after_effect": "'$after_effect'", "normalization": "'$normalization'", "normalization.h2.c": "3.0" } } }, "mappings": {  "doc":{  "properties": {  "src": {  "type": "string",  "similarity": "dfr_similarity"  }  }  }  } ,  "index" : {  "analysis" : { "analyzer" : { "default" : { "type" : "whitespace" } } } } }' > /dev/null
 
 cd $HOME
-./scripts/readbulk.sh localhost $input $final_index_name $doctype $norm $n true > /dev/null
+./scripts/read.sh localhost $input "$final_index_name/$doctype" $norm $n true > /dev/null
+
+
+# do a refresh to make all index available
+#curl -XPOST $hostname:9200/$final_index_name/_refresh > /dev/null
+#echo "Before sleep"
+sleep 10
+#echo "After sleep"
 
 # use DFS mode
 dfs="-f"
