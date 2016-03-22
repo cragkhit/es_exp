643f03fb17d4264294b409472ac785faf20e985d
Chaiyong Ragkhitwetsagul <cragkhit@gmail.com>
2016-03-22 20:31:15 +0000

    Updated JavaTokenizer and scripts'

--- scripts/eval_dfr.sh
+++ scripts/eval_dfr.sh
@@ -1,10 +1,8 @@
 # A script to run ES with the given normalisation
 # This script creates DFR index with different parameter settings of basic_model, after_effect, and normalization
 
-HOME="/Users/Chaiyong/Documents/es_exp/"
-
 if [ "$#" -eq 0 ]; then 
-        echo "Usage: ./best_bm25.sh <1. input folder> <2. index prefix> <3. size of n> <4. normalisation> <5. output folder> <6.basic_model> <7. after_effect> <8. normalization>" 
+        echo "Usage: ./best_bm25.sh <1. input folder> <2. index prefix> <3. size of n> <4. normalisation> <5. output folder> <6.basic_model> <7. after_effect> <8. normalization> <9. working dir>" 
         exit 1
 fi
 
@@ -18,16 +16,17 @@ output=$5
 basic_model=$6
 after_effect=$7
 normalization=$8
+HOME=$9
 
 
 final_index_name=$index"_"$basic_model"_"$after_effect"_"$normalization
 
 #echo "======================================================="
-#  printf "HOST:\t\t\t$hostname\n"
-#  printf "INDEX:\t\t\t$final_index_name/$doctype\n"
-#  printf "NORM:\t\t\t$norm + $n-gram\n"
-#  printf "SETTINGS:\t\tbasic_model=$basic_model, after_effect=$after_effect, normalization=$normalization\n"
-#  printf "DATA:\t\t\tfrom: $input to: $output\n"
+#printf "HOST:\t\t\t$hostname\n"
+#printf "INDEX:\t\t\t$final_index_name/$doctype\n"
+#printf "NORM:\t\t\t$norm + $n-gram\n"
+#printf "SETTINGS:\t\tbasic_model=$basic_model, after_effect=$after_effect, normalization=$normalization\n"
+#printf "DATA:\t\t\tfrom: $input to: $output\n"
 #echo "======================================================="
 
 curl -s -XDELETE $hostname:9200/$final_index_name > /dev/null
@@ -38,8 +37,12 @@ cd $HOME
 
 # use DFS mode
 dfs="-f"
-mkdir $output
-java -jar tools/checker_evo.jar -s $hostname -i $final_index_name -t $doctype -d $input -l $norm $dfs -n -g $n #1> $output/$final_index_name.csv
+
+if [ ! -d "$output" ]; then
+    mkdir $output
+fi
+
+java -jar tools/checker_evo.jar -s $hostname -i $final_index_name -t $doctype -d $input -l $norm $dfs -n -g $n #1> $output/$final_index_name.csv 
 
   # clean up by delete the index
 curl -s -XDELETE $hostname:9200/$final_index_name > /dev/null
--- scripts/read.sh
+++ scripts/read.sh
@@ -1,16 +1,23 @@
 TOKENIZER="tools/java_tokenizer.jar"
 if [ "$#" -eq 0 ]; then
-	echo "Usage: ./read.sh <input dir> <index/type> <norm level (w/d/j/p/k/s/e)> <n value of ngram> <ngram (true/false)>"
+	echo "Usage: ./read.sh <hostname> <input dir> <index/type> <norm level (w/d/j/p/k/s/e)> <n value of ngram> <ngram (true/false)>"
 	exit 1
 fi
-for file in $1/**/*.java
+
+hostname=$1
+inputDir=$2
+index=$3
+norm=$4
+n=$5
+ngram=$6
+
+for file in $inputDir/*.java
 do
     #whatever you need with "$file"
     id=`basename $file | sed -e 's/.java//'`
     #echo $id
-    src=`java -jar "$TOKENIZER" -f $file -l $3 -v $4 -n $5`
+    src=`java -jar "$TOKENIZER" -f $file -l $norm -v $n -n $ngram`
     #echo "src="$src
     printf $id", "
-    #echo "curl -XPUT 'http://localhost:9200/$2/$id' -d '{ \"id\" : \"$id\", \"src\" : \"$src\"}'"
-    curl -s -XPUT "http://localhost:9200/$2/$id?pretty" -d "{ \"id\" : \"$id\", \"src\" : \"$src\"}" > /dev/null
+    curl -s -XPUT "http://$hostname:9200/$index/$id?pretty" -d "{ \"id\" : \"$id\", \"src\" : \"$src\"}" > /dev/null
 done
