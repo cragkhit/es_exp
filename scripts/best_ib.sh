160232931d65d11c3481d3fb56c8efcf42071444
Chaiyong R <cragkhit@gmail.com>
2016-03-24 12:16:02 +0000

    Updated IB scripts

--- scripts/best_ib.sh
+++ scripts/best_ib.sh
@@ -1,24 +1,46 @@
+# A script to run ES with the given normalisation
+# This script creates IB index with different parameter settings of distribution, lambda, and normalization
+
 if [ "$#" -eq 0 ]; then
-        echo "Usage: ./run_all.sh <server name> <input folder> <ES doc type> <index prefix> <normalization>" 
+        echo "Usage: ./best_bm25.sh <server name> <input folder> <ES doc type> <index prefix> <normalization> <size of n> <output folder>" 
         exit 1
 fi
+    hostname=$1
+    input=$2
+    doctype=$3
+    index=$4
+    norm=$5
+    n=$6
+    output=$7
 
 # create all possible index combinations
 for i in "ll" "spl"; do
     for j in "df" "ttf"; do
         for k in "no" "h1" "h2" "h3" "z"; do
           final_index_name=$4"_"$i"_"$j"_"$k
-          curl -XDELETE $1:9200/$final_index_name
-          curl -XPUT $1:9200/$final_index_name -d '{ "settings": { "similarity": { "ib_similarity" : { "type": "IB", "distribution": "'$i'", "lambda": "'$j'", "normalization": "'$k'"} } }, "mappings": {  "doc":{  "properties": {  "src": {  "type": "string",  "similarity": "ib_similarity"  }  }  }  } ,  "index" : {  "analysis" : { "analyzer" : { "default" : { "type" : "whitespace" } } } } }'
 
-          ./scripts/readbulk.sh $2 "$final_index_name" "$3" $5 4 true
+            echo "======================================================="
+            printf "HOST:\t\t\t$hostname\n"
+            printf "INDEX:\t\t\t$final_index_name/$doctype\n"
+            printf "NORM:\t\t\t$norm + $n-gram\n"
+            printf "SETTINGS:\t\tdistribution=$i, lambda=$j, normalization=$k\n"
+            printf "DATA:\t\t\tfrom: $input to: $output\n"
+            echo "======================================================="
 
-          dfs="-f"
-          output=best_param_ib
-          mkdir $output
-          #echo "java -jar tools/checker.jar -s $1 -i $final_index_name -t src -d $3 -l $i $ngram_flag $k 1> $output/$final_index_name.csv"
-          java -jar tools/checker.jar -s $1 -i $final_index_name -t $3 -d $2 -l $5 -n $dfs 1> $output/$5ngram_$final_index_name.csv
-          curl -XDELETE $1:9200/$final_index_name
+            curl -XDELETE $hostname:9200/$final_index_name
+            curl -XPUT $hostname:9200/$final_index_name -d '{ "settings": { "similarity": { "ib_similarity" : { "type": "IB", "distribution": "'$i'", "lambda": "'$j'", "normalization": "'$k'"} } }, "mappings": {  "doc":{  "properties": {  "src": {  "type": "string",  "similarity": "ib_similarity"  }  }  }  } ,  "index" : {  "analysis" : { "analyzer" : { "default" : { "type" : "whitespace" } } } } }'
+             ./scripts/read.sh $hostname $input "$final_index_name/$doctype" $norm $n true > /dev/null
+
+            # use DFS mode
+            dfs="-f"
+            if [ ! -d $output ]; then
+                mkdir $output
+            fi
+            java -jar tools/checker.jar -s $hostname -i $final_index_name -t $doctype -d $input -l $norm $dfs -n -g $n 1> $output/$final_index_name.csv
+
+            # clean up by delete the index
+            curl -XDELETE $hostname:9200/$final_index_name
         done
     done
 done
+
---
+++ scripts/norm_all_ib.sh
@@ -0,0 +1,16 @@
+if [ "$#" -eq 0 ]; then
+        echo "Usage: ./norm_all_ib.sh <server name> <input folder> <ES doc type> <index prefix> <output folder>" 
+        exit 1
+fi
+
+    hostname=$1
+    input=$2
+    doctype=$3
+    index=$4
+    output=$5
+
+for i in "s" "w" "ws" "p" "ps" "pw" "pws" "k" "ks" "kw" "kws" "kp" "kps" "kpw" "kpws" "j" "js" "jw" "jws" "jp" "jps" "jpw" "jpws" "jk" "jks" "jkw" "jkws" "jkp" "jkps" "jkpw" "jkpws" "d" "ds" "dw" "dws" "dp" "dps" "dpw" "dpws" "dk" "dks" "dkw" "dkws" "dkp" "dkps" "dkpw" "dkpws" "dj" "djs" "djw" "djws" "djp" "djps" "djpw" "djpws" "djk" "djks" "djkw" "djkws" "djkp" "djkps" "djkpw" "djkpws"; do
+    for n in 2 3 4 5; do
+        ./scripts/best_ib.sh $hostname $input $doctype $index $i $n $output
+    done
+done
